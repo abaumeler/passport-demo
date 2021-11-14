3221a4eb13c38e8b6fe77632ea479ca43daba095
Andres Baumeler <9219885+abaumeler@users.noreply.github.com>
2021-11-14 13:30:07 +0100

    user registration working

--- Readme.md
+++ Readme.md
@@ -1,2 +1,13 @@
 # passport-demo
 Demo of the passport.js framework
+
+## Requirements
+This app uses ejs views and the multer middleware to parse the multipart/form-data sent by the forms.
+- the forms must have set the `enctype="multipart/form-data"`
+- ejs must be placed in the dir specified on the line `app.set('views', './views');` in server.js
+
+## DB Setup
+Mongo DB can be run in a docker container like this:
+```
+docker run --name mongodb -d -p 27017:27017 mongo
+```
--- server.js
+++ server.js
@@ -1,43 +1,53 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const mongoose = require ('mongoose')
+const multer = require('multer');
+const upload = multer();
 
+// Mongo DB Setup
 mongoose.connect('mongodb://localhost:27017/passport-demo')
 mongoose.set('debug', true);
 
 require('./models/User')
-var User = mongoose.model('User');
+let User = mongoose.model('User');
 
-// Create global app object
-const app = express()
-app.set('view-engine', 'ejs')
+// Create global Express JS app object
+const app = express();
+app.set('views', './views');
+app.set('view-engine', 'ejs');
 
+// Route handling
 app.get('/', (req, res) => {
     res.redirect('/login');
-})
+});
 
 app.get('/login', (req, res) => {
     res.render('login.ejs');
-})
+});
 
 app.get('/register', (req, res) => {
     res.render('register.ejs');
-})
+});
 
-app.post('/register', (req, res) => {
-   var user = new User();
-   //user.name = req.header.name;
-   //user.email = req.header.email;
-   //user.setPassword(req.header.password)
-   user.name = 'a';
-   user.emai = 'a@a';
-   user.setPassword('a')
-
+app.post('/register', upload.none(), (req, res) => {
+   console.log(req.body);
+   let user = new User();
+   user.name = req.body.name;
+   user.email = req.body.email;
+   user.setPassword(req.body.password)
    user.save().then(function(){console.log('user saved')});
+   res.render('login.ejs');
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Error handlers
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     console.log(err.stack);
 
     res.status(err.status || 500);
