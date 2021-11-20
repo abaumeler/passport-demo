03156f714c12a50d0e97ce1530b199ad991ac989
Andres Baumeler <9219885+abaumeler@users.noreply.github.com>
2021-11-20 11:59:50 +0100

    restructured user verify function

--- passport-config.js
+++ passport-config.js
@@ -3,15 +3,20 @@ const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 let User = mongoose.model('User');
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, function (email, password, done) {
-  User.findOne({ email: email }).then(function (user) {
+
+// check if a user exists for a given e-mail
+// if the user exists verify the password
+// if password is correct, return user
+userVerifyFunction = (email, password, done) => {
+  User.findOne({ email: email }).then(user => {
     if (!user || !user.validPassword(password)) {
       return done(null, false, { errors: { 'email or password': 'is invalid' } });
     }
-
     return done(null, user);
   }).catch(done);
-}));
+}
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, userVerifyFunction));
