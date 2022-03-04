c57f555e4d6dbb42736fb4a78894803ac06c48e4
Andres Baumeler <9219885+abaumeler@users.noreply.github.com>
2022-03-04 13:12:55 +0100

    - changed token header

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('express-jwt');
 const env = require('dotenv').config();
 
 function getTokenFromHeader(req) {
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
         return req.headers.authorization.split(' ')[1];
     }
 
